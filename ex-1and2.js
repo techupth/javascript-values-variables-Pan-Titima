6cc1f8901709fde3928e94815d91dd106e169530
Pan-Titima <32396409+Pan-Titima@users.noreply.github.com>
2024-03-20 14:37:01 +0700

    Update ex-1and2.js
    Add answer for ex-1 and 2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let personName = "Karter";
+let personSurName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurant = "Chef's Table by Chef James";
+const billTotal = 15000;
+const restaurantActive = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+// can not reassign value for const billTotal
+// can not reassign value for const restaurantActive
+
